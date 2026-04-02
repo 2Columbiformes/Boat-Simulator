7a2d3b4ec44da6b0fc51f8bb32dc866067103b05
2Columbiformes <wenan2030@gmail.com>
2026-04-01 16:18:24 -1000

    Update asf.py

--- asf.py
+++ asf.py
@@ -1,1 +1,7 @@
-print("chat is bad")
+print("chat is ehf")
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
